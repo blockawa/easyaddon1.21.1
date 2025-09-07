b9caa9631b667008aa621c03f17194e3a30229f6
blockawa <1020881911@qq.com>
2025-09-08 00:21:04 +0800

    Add files via upload

--- src/main/java/me/idhammai/addon/modules/AutoLoginXin.java
+++ src/main/java/me/idhammai/addon/modules/AutoLoginXin.java
@@ -15,23 +15,16 @@ import net.minecraft.item.Items;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 
+
 public class AutoLoginXin extends Module {
     private final MinecraftClient mc = MinecraftClient.getInstance();
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     public static AutoLoginXin INSTANCE;
-
-    /* Timers */
     private final Timer queueTimer = new Timer();
     private final Timer timer = new Timer();
     private final Timer containerTimer = new Timer();
-    private final Timer queueUseTimer = new Timer();
-
-    /* States */
     private boolean login = false;
-    private boolean hasJoinedQueue = false;
-    private boolean waitingForQueueUse = false;
 
-    /* Settings */
     private final Setting<String> password = sgGeneral.add(new StringSetting.Builder()
         .name("登录密码")
         .description("Xin服登录密码")
@@ -43,7 +36,10 @@ public class AutoLoginXin extends Module {
         .name("输入密码延迟")
         .description("输入密码前等待的时间，单位秒")
         .defaultValue(2)
-        .min(0).max(10).sliderMin(0).sliderMax(10)
+        .min(0)
+        .max(10)
+        .sliderMin(0)
+        .sliderMax(10)
         .build()
     );
 
@@ -51,15 +47,10 @@ public class AutoLoginXin extends Module {
         .name("加入队列延迟")
         .description("右键指南针加入队列等待的时间，单位秒")
         .defaultValue(2)
-        .min(0).max(10).sliderMin(0).sliderMax(10)
-        .build()
-    );
-
-    public final Setting<Integer> queueUseDelay = sgGeneral.add(new IntSetting.Builder()
-        .name("右键后延迟")
-        .description("右键使用指南针后等待多少秒才标记为已加入队列")
-        .defaultValue(1)
-        .min(0).max(5).sliderMin(0).sliderMax(5)
+        .min(0)
+        .max(10)
+        .sliderMin(0)
+        .sliderMax(10)
         .build()
     );
 
@@ -67,7 +58,10 @@ public class AutoLoginXin extends Module {
         .name("容器点击延迟")
         .description("在容器中点击指南针的延迟时间，单位秒")
         .defaultValue(2)
-        .min(0).max(10).sliderMin(0).sliderMax(10)
+        .min(0)
+        .max(10)
+        .sliderMin(0)
+        .sliderMax(10)
         .build()
     );
 
@@ -87,21 +81,24 @@ public class AutoLoginXin extends Module {
     public void onTick(TickEvent.Pre event) {
         if (mc.player == null) return;
 
-        /* 登录逻辑 */
         if (login && timer.passedS(afterLoginTime.get())) {
+            System.out.println("login" + password.get());
             mc.getNetworkHandler().sendChatCommand("login " + password.get());
             login = false;
         }
 
-        /* 只在登录大厅且未加入队列时执行 */
-        if (isInLoginLobby() && !hasJoinedQueue && !waitingForQueueUse) {
-            /* 容器内点击指南针 */
+        // 只在未完成登录且在登录大厅位置时执行队列相关操作
+        if (isInLoginLobby()) {
+            // 处理容器界面中的指南针点击
             if (mc.currentScreen instanceof GenericContainerScreen && containerTimer.passedS(containerClickDelay.get())) {
                 GenericContainerScreen containerScreen = (GenericContainerScreen) mc.currentScreen;
                 var handler = containerScreen.getScreenHandler();
+
+                // 查找容器中的指南针
                 for (int i = 0; i < handler.slots.size(); i++) {
                     var slot = handler.slots.get(i);
                     if (slot.hasStack() && slot.getStack().getItem() == Items.COMPASS) {
+                        // 点击指南针
                         mc.interactionManager.clickSlot(handler.syncId, i, 0, SlotActionType.PICKUP, mc.player);
                         containerTimer.reset();
                         break;
@@ -109,21 +106,11 @@ public class AutoLoginXin extends Module {
                 }
             }
 
-            /* 快捷栏右键指南针加入队列 */
-            var compass = InvUtils.find(Items.COMPASS);
-            if (compass.isHotbar() && queueTimer.passedS(joinQueueDelay.get())) {
-                InvUtils.swap(compass.slot(), false);
+            if (InvUtils.find(Items.COMPASS).isHotbar() && queueTimer.passedS(joinQueueDelay.get())) {
+                InvUtils.swap(InvUtils.find(Items.COMPASS).slot(),false);
                 mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
                 queueTimer.reset();
-                waitingForQueueUse = true;
-                queueUseTimer.reset();
             }
-        }
-
-        /* 等待右键生效后标记为已加入队列 */
-        if (waitingForQueueUse && queueUseTimer.passedS(queueUseDelay.get())) {
-            hasJoinedQueue = true;
-            waitingForQueueUse = false;
         }
     }
 
@@ -133,8 +120,6 @@ public class AutoLoginXin extends Module {
             login = true;
             timer.reset();
             containerTimer.reset();
-            hasJoinedQueue = false;
-            waitingForQueueUse = false;
         }
     }
 }
